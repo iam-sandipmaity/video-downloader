c318d6bc8afe58e7613dd37712f7e3f9fea0a8e1
Sandip Maity <social.sandipmaity+github@gmail.com>
2026-04-07 03:37:55 +0530

    feat(theme): replace brand palette with ember and ash colors

--- app/src/main/java/com/localdownloader/ui/theme/Color.kt
+++ app/src/main/java/com/localdownloader/ui/theme/Color.kt
@@ -2,9 +2,19 @@ package com.localdownloader.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val BrandPrimary = Color(0xFF0B57D0)
-val BrandOnPrimary = Color(0xFFFFFFFF)
-val BrandSecondary = Color(0xFF00897B)
-val BrandBackground = Color(0xFFF7F9FC)
-val BrandSurface = Color(0xFFFFFFFF)
-val BrandError = Color(0xFFB3261E)
+val Ember = Color(0xFFB55311)
+val EmberContainer = Color(0xFF5B2900)
+val AshBackground = Color(0xFF17131C)
+val AshSurface = Color(0xFF24202C)
+val AshSurfaceVariant = Color(0xFF534D5D)
+val AshOutline = Color(0xFF8F889B)
+val MoonText = Color(0xFFF3EDF8)
+val LimeAccent = Color(0xFF65E11A)
+val Mist = Color(0xFFD0C7DB)
+
+val DawnBackground = Color(0xFFF6F1EA)
+val DawnSurface = Color(0xFFFFFBFF)
+val DawnSurfaceVariant = Color(0xFFE7DDEA)
+val DawnText = Color(0xFF241F2A)
+val DawnSecondary = Color(0xFF6B5F7B)
+val CoralError = Color(0xFFBA1A1A)
