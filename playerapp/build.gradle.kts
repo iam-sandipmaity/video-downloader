6c7294b6200592e59344139cdda62b56e4e92d01
iam-sandipmaity <social.sandipmaity+github@gmail.com>
2026-05-02 03:39:57 +0530

    Revert "Fix standalone player Material theme dependency"
    This reverts commit aca094279f816663ce1fb3ee4d8a23bf1e47d598.

--- playerapp/build.gradle.kts
+++ playerapp/build.gradle.kts
@@ -72,7 +72,6 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-extended")
-    implementation("com.google.android.material:material:1.12.0")
 
     implementation("androidx.media3:media3-exoplayer:1.5.1")
     implementation("androidx.media3:media3-ui:1.5.1")
