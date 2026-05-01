91c61aed36a46adb5b5bd89b33eedee60eb8d6b9
iam-sandipmaity <social.sandipmaity+github@gmail.com>
2026-05-02 03:08:07 +0530

    Bump release to 1.5.7

--- playerapp/build.gradle.kts
+++ playerapp/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "0.1.0"
+        versionName = "1.5.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
