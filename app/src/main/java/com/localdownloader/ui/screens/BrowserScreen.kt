8fd05591e09eb52a8c29803003b17a9d30d788ab
iam-sandipmaity <social.sandipmaity+github@gmail.com>
2026-05-21 23:44:15 +0530

    Fix BrowserScreen weight import

--- app/src/main/java/com/localdownloader/ui/screens/BrowserScreen.kt
+++ app/src/main/java/com/localdownloader/ui/screens/BrowserScreen.kt
@@ -27,7 +27,6 @@ import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.navigationBarsPadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.ui.input.nestedscroll.nestedScroll
