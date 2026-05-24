fc409cd7c17e5287f9fecee6fe3567fdc3939885
iam-sandipmaity <social.sandipmaity+github@gmail.com>
2026-05-25 02:25:34 +0530

    Fix player screen weight import

--- app/src/main/java/com/localdownloader/ui/screens/PlayerScreen.kt
+++ app/src/main/java/com/localdownloader/ui/screens/PlayerScreen.kt
@@ -35,7 +35,6 @@ import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.statusBarsPadding
-import androidx.compose.foundation.layout.weight
 import androidx.compose.foundation.layout.widthIn
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
